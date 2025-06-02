ea9a93ad1b7b64fccbe2bb1fac1aa45dd31f7814
Tabb <tabbes691@gmail.com>
2025-06-02 12:43:04 +0100

    Initial Uploads

---
+++ src/main/java/com/tabbeys/dimensioncraft/DimensionCraft.java
@@ -0,0 +1,10 @@
+package com.tabbeys.dimensioncraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("dimensioncraft")
+public class DimensionCraft {
+    public DimensionCraft() {
+        // Mod initialization code here
+    }
+}
